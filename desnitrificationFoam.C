b1831eff7ec14a84557fc49f8524873747c0c7fb
ANDRES ORELLANA <aeorellana1@miuandes.cl>
2025-12-27 02:57:23 -0300

    Initial commit: desnitrificationFoam solver (OpenFOAM v2406)

---
+++ desnitrificationFoam.C
@@ -0,0 +1,131 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2019 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    biofilmPimpleFoam
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "dynamicFvMesh.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+#include "pimpleControl.H"
+//#include "CorrectPhi.H"
+#include "cfdTools/general/CorrectPhi/CorrectPhi.H"
+#include "fvOptions.H"
+#include "timestepManager.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createDynamicFvMesh.H"
+    #include "initContinuityErrs.H"
+    #include "createDyMControls.H"
+    
+    #include "createFieldsBiofilm.H"
+    #include "createFieldsNOx.H"    
+    #include "createFields.H"
+
+    #include "createUfIfPresent.H"
+    #include "CourantNo.H"
+
+    #include "readTimeControls.H"
+    // Permite activar/desactivar la ecuación de C desde system/controlDict
+    const Switch useC = runTime.controlDict().lookupOrDefault<Switch>("useC", true);
+    // Permite activar/desactivar la convección de NO3, NO2, Csb y SO4
+    const Switch advectSolutes = runTime.controlDict().lookupOrDefault<Switch>("advectSolutes", true);
+
+    #include "setInitialDeltaT.H"
+    
+    turbulence->validate();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+  
+    Info << "\nStarting time loop\n" << endl;
+    
+    Info<< nl
+        << "ADVECTIVE SOLUTE TRANSPORT: " << (advectSolutes ? "ON" : "OFF") << nl
+        << "useC: " << (useC ? "ON" : "OFF") << nl << endl;
+
+    while (runTime.run())
+    {
+	
+	#include "CourantNo.H"
+	#include "setDeltaT.H"
+      
+	++runTime;
+
+	Info << "Time = " << runTime.timeName() << nl << endl;
+	
+	#include "pimple.H"
+        if (useC)
+        {
+            #include "CEqn.H"
+        }
+
+	#include "updateDiffusion.H"
+        #include "MHidEqn.H"
+	#include "NOxEqn.H"
+	#include "MEqn.H"
+        #include "updateInterfaceMS.H"
+	#include "updateBiofilmPhase.H"
+
+        if (useC) dtManagerC.updateDerivatives();
+	dtManagerM.updateDerivatives();
+        dtManagerNO3.updateDerivatives();
+        dtManagerNO2.updateDerivatives();
+
+        if (useC)
+        {
+            Info << "C in [" << gMin(C) << ", " << gMax(C) << "]" << endl;
+        }
+	
+        Info << "M in [" << gMin(M) << ", " << gMax(M) << "]" << endl;
+        Info << "NO3 in [" << gMin(NO3) << ", " << gMax(NO3) << "]" << endl;
+        Info << "NO2 in [" << gMin(NO2) << ", " << gMax(NO2) << "]" << endl;
+        Info << "Csb in [" << gMin(Csb) << ", " << gMax(Csb) << "]" << endl;
+        Info << "SO4 in [" << gMin(SO4) << ", " << gMax(SO4) << "]" << endl;
+        Info << "Mhid in [" << gMin(Mhid) << ", " << gMax(Mhid) << "]" << endl;
+        Info << "S0  in [" << gMin(S0)  << ", " << gMax(S0)  << "]" << endl;
+	runTime.write();
+
+	runTime.printExecutionTime(Info);
+    }
+
+    Info << "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
